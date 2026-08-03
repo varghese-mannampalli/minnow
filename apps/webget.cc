c235394e6a571f489ed527c9ea3b4c0022a6f2f3
Varghese Mannampalli <varghese@MBAVM.local>
2026-08-03 18:14:29 +0200

    Lab0 parametrizing webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -11,8 +11,10 @@ void get_URL( const string& host, const string& path )
 {
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
   TCPSocket sock;
-  sock.connect(Address("127.0.0.1", 8080));
-  sock.write("GET /lab0/vmann HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nConnection: close\r\n\r\n");
+  sock.connect(Address(host, "http"));
+  sock.write("GET " + path + " HTTP/1.1\r\n"
+    "Host: " + host + "\r\n"
+    "Connection: close\r\n\r\n");
   std::string buff;
   while (!sock.eof()) {
     sock.read(buff);
